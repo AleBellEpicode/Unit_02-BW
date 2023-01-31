85300e6f235165a0ab58939f22c6dd668289e6f5
Vincenzo M <vincenzomaiorana@gmail.com>
2023-01-31 10:35:59 +0100

    searchQuery function

--- assets/script/connect.js
+++ assets/script/connect.js
@@ -7,20 +7,49 @@ const fetchByQuery = async (type = 'query', query) => {
    switch (type) {
       case 'album':
          res = await fetch(`${APIUrl}album/${query}`)
-         const albums = await res.json()
-         return await albums
+         try {
+            if (res.ok) {
+               const albums = await res.json()
+               return await albums
+            }
+         } catch (err) {
+            console.log('Error while fetching')
+         }
          break;
 
       case 'artist':
          res = await fetch(`${APIUrl}artist/${query}`)
-         const artist = await res.json()
-         return await artist
+         try {
+            if (res.ok) {
+               const artist = await res.json()
+               return await artist
+            }
+         } catch (err) {
+            console.log('Error while fetching')
+         }
          break;
 
       default:
          res = await fetch(`${APIUrl}search?q=${query}`)
-         const { data: songs } = await res.json()
-         return await songs;
+         try {
+            if (res.ok) {
+               const { data: songs } = await res.json()
+               return await songs;
+            }
+         } catch (err) {
+            console.log('Error while fetching')
+         }
          break;
    }
+}
+
+const searchQuery = async (string) => {
+   let songs = await fetchByQuery('query', string)
+   let albums = []
+   await songs.forEach(element => {
+      albums.push(element.album)
+   });
+   return await { songs, albums }
+   /*  DEBUG  await console.log({ songs, albums }) */
+   /* utilizzo: await searchQuery('ricerca') */
 }
