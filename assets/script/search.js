e2397d15f043b69ade27d246dc5908d8586299de
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-03 10:48:06 +0100

    serachpage player and link to album/artist

--- assets/script/search.js
+++ assets/script/search.js
@@ -26,9 +26,8 @@ const fetchByQuery = async (query) => {
       artistList.innerHTML = ''
       albumsList.innerHTML = ''
       for (let i = 0; i < popularNo; i++) {
-         renderTracks(songs[i])
+         renderTracks(songs[i], i + 1)
          renderAlbums(songs[i].album, songs[i].artist)
-
       }
       //altri album dell'artista
       renderArtists(songs[0].artist)
@@ -38,10 +37,10 @@ const fetchByQuery = async (query) => {
    }
 }
 
-const renderTracks = (track) => {
-   trackList.innerHTML += `<div class="songContainer">
+const renderTracks = (track, i) => {
+   trackList.innerHTML += `<div class="songContainer" song-id="${track.id}" onclick="setPlayer(event)">
    <div class="number-title-song">
-     <span class="num">1</span>
+     <span class="num">${i}</span>
      <p>${track.title_short} - ${track.artist.name}</p>
    </div>
    <div class="songDuration">
@@ -49,23 +48,23 @@ const renderTracks = (track) => {
    </div>
  </div>`
 }
-// ${track.id}
 
 const renderArtists = (artist) => {
-   artistList.innerHTML += `<div class="img-artist-container">
-   <img src="${artist.picture_big}" alt="" class="w-100 rounded-circle">
- </div>
- <div>
-   <p class="nameArtist">${artist.name}</p>
- </div>
- <div>
-   <span class="tagArtist">${artist.id}</span>
- </div>
- <div class="btn-play-hover">
-   <a class="">
-     <i class="bi bi-play-circle-fill suggestedPlaylist-playButton position-absolute playSearchCard"></i>
-   </a>
- </div>`
+   artistList.innerHTML += `
+   <div class="img-artist-container" onclick="location.href='artistpage.html?artistId=${artist.id}';">
+      <img src="${artist.picture_big}" alt="" class="w-100 rounded-circle">
+   </div>
+   <div>
+      <p class="nameArtist">${artist.name}</p>
+   </div>
+   <div>
+      <span class="tagArtist">Artista</span>
+   </div>
+   <div class="btn-play-hover">
+      <a class="">
+         <i class="bi bi-play-circle-fill suggestedPlaylist-playButton position-absolute playSearchCard"></i>
+      </a>
+   </div>`
 }
 const durationFormat = (duration) => {
    // Hours, minutes and seconds
@@ -90,12 +89,12 @@ const durationFormat = (duration) => {
 }
 
 const renderAlbums = (album, artist) => {
-   albumsList.innerHTML += `<div class="card col p-3 p-0 playing-card" style="width: 11rem" album-id="${album.id}">
-   <img src=" ${album.cover_big}" class="card-img-top position-relative" alt="..." />
+   albumsList.innerHTML += `<div class="card col p-3 p-0 playing-card" style="width: 11rem">
+   <img src=" ${album.cover_big}" class="card-img-top position-relative" alt="..."  onclick="location.href='albumpage.html?albumId=${album.id}';" />
    <a class="">
-     <i class="bi bi-play-circle-fill suggestedPlaylist-playButton position-absolute"></i>
+     <i class="bi bi-play-circle-fill suggestedPlaylist-playButton position-absolute" album-id="${album.id}" onclick="setPlayer(event)"></i>
    </a>
-   <div class="card-body">
+   <div class="card-body" onclick="location.href='albumpage.html?albumId=${album.id}';">
      <h6 class="card-title">${album.title}</h6>
      <p class="card-text text-secondary">
      ${artist.name}
